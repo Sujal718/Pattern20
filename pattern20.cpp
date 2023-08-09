0798ad7c87305dc344a1c02d11257757ad9ffad6
Sujal718 <104685635+Sujal718@users.noreply.github.com>
2023-08-10 01:08:10 +0530

    Add files via upload

---
+++ pattern20.cpp
@@ -0,0 +1,29 @@
+/*
+
+****
+***
+**
+*
+
+*/
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n,i=1;
+    cin>>n;
+    while(i<=n){
+      int star= n-i+1;
+           while(star){
+            cout<<'*';
+            star=star-1;
+           }
+         int j=1;
+        while(j<=n){
+            j=j+1;
+        }
+        cout<<endl;
+        i=i+1;
+    }
+}
